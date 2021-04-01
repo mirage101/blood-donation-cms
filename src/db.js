b89f3437c37681d519f70efe5a86365885483720
Ivan Filipov <mirage101@abv.bg>
2021-04-01 10:55:14 +0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,11 +2,11 @@
 import firebase from 'firebase';
 
  const firebaseConfig = {
-    apiKey: "AIzaSyATIptf6cHr5cQ4-3LAAiMPOWCjlhTXrMc",
-    authDomain: "vue-spas-ffe28.firebaseapp.com",
-    databaseURL: "https://vue-spas-ffe28.firebaseio.com",
-    projectId: "vue-spas-ffe28",
-    storageBucket: "vue-spas-ffe28.appspot.com",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
     messagingSenderId: "329658329646",
     appId: "1:329658329646:web:f6079be9f7502a17760dbf"
   };
